a8e7b3a9e8e6513bd710d57649e937afd8916dba
João Vitor <52049527+JoaoVitor2344@users.noreply.github.com>
2021-03-25 22:25:58 -0300

    Add files via upload

---
+++ frmExLista.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace ExLista
+{
+    partial class frmExLista
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbResultado = new System.Windows.Forms.ListBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnFiltro = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnLocalizar = new System.Windows.Forms.Button();
+            this.lblId = new System.Windows.Forms.Label();
+            this.btnLocalizar2 = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnGravaBanco = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.FormattingEnabled = true;
+            this.lbResultado.Location = new System.Drawing.Point(12, 12);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(572, 225);
+            this.lbResultado.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(12, 243);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(273, 62);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar Lista";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnFiltro
+            // 
+            this.btnFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiltro.Location = new System.Drawing.Point(12, 311);
+            this.btnFiltro.Name = "btnFiltro";
+            this.btnFiltro.Size = new System.Drawing.Size(273, 62);
+            this.btnFiltro.TabIndex = 2;
+            this.btnFiltro.Text = "Mostrar Pessoas +30 anos";
+            this.btnFiltro.UseVisualStyleBackColor = true;
+            this.btnFiltro.Click += new System.EventHandler(this.btnFiltro_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(12, 379);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(273, 62);
+            this.btnRemover.TabIndex = 3;
+            this.btnRemover.Text = "Remover todos do Sexo masculino";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // btnLocalizar
+            // 
+            this.btnLocalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizar.Location = new System.Drawing.Point(311, 243);
+            this.btnLocalizar.Name = "btnLocalizar";
+            this.btnLocalizar.Size = new System.Drawing.Size(273, 62);
+            this.btnLocalizar.TabIndex = 4;
+            this.btnLocalizar.Text = "Locaizar os objetos com idade de 12";
+            this.btnLocalizar.UseVisualStyleBackColor = true;
+            this.btnLocalizar.Click += new System.EventHandler(this.btnLocalizar_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(620, 12);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 5;
+            this.lblId.Text = "Id";
+            // 
+            // btnLocalizar2
+            // 
+            this.btnLocalizar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocalizar2.Location = new System.Drawing.Point(819, 12);
+            this.btnLocalizar2.Name = "btnLocalizar2";
+            this.btnLocalizar2.Size = new System.Drawing.Size(75, 23);
+            this.btnLocalizar2.TabIndex = 6;
+            this.btnLocalizar2.Text = "Localizar";
+            this.btnLocalizar2.UseVisualStyleBackColor = true;
+            this.btnLocalizar2.Click += new System.EventHandler(this.btnLocalizar2_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(686, 12);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(686, 57);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(208, 20);
+            this.txtNome.TabIndex = 9;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(620, 57);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 8;
+            this.lblNome.Text = "Nome";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(686, 102);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 11;
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Location = new System.Drawing.Point(620, 102);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(34, 13);
+            this.lblIdade.TabIndex = 10;
+            this.lblIdade.Text = "Idade";
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(686, 147);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(100, 20);
+            this.txtSexo.TabIndex = 13;
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(620, 147);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(31, 13);
+            this.lblSexo.TabIndex = 12;
+            this.lblSexo.Text = "Sexo";
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(623, 214);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 14;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(724, 214);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 15;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(819, 214);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 16;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnGravaBanco
+            // 
+            this.btnGravaBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravaBanco.Location = new System.Drawing.Point(623, 263);
+            this.btnGravaBanco.Name = "btnGravaBanco";
+            this.btnGravaBanco.Size = new System.Drawing.Size(271, 23);
+            this.btnGravaBanco.TabIndex = 17;
+            this.btnGravaBanco.Text = "Gravar no Banco de Dados";
+            this.btnGravaBanco.UseVisualStyleBackColor = true;
+            this.btnGravaBanco.Click += new System.EventHandler(this.btnGravaBanco_Click);
+            // 
+            // frmExLista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 464);
+            this.Controls.Add(this.btnGravaBanco);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnLocalizar2);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnLocalizar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnFiltro);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lbResultado);
+            this.Name = "frmExLista";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exemplo de Lista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbResultado;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnFiltro;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnLocalizar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Button btnLocalizar2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnGravaBanco;
+    }
+}
+
---
+++ frmExLista.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ExLista
+{
+    public partial class frmExLista : Form
+    {
+        SqlConnection con = new SqlConnection("");
+        List<Pessoa> pLista = null;
+
+        public frmExLista()
+        {
+            InitializeComponent();
+        }
+
+        //Adicionar objetos a miha lista
+        private void CarregaLista()
+        {
+            pLista = new List<Pessoa>();
+            pLista.Add(new Pessoa(1, "João", 12, 'M'));
+            pLista.Add(new Pessoa(2, "Maria", 12, 'F'));
+            pLista.Add(new Pessoa(3, "Roberto", 89, 'M'));
+            pLista.Add(new Pessoa(4, "Mari", 20, 'F'));
+            pLista.Add(new Pessoa(5, "Valdir", 20, 'M'));
+            pLista.Add(new Pessoa(6, "Rodrigo", 47, 'M'));
+        }
+
+        void Imprimir(List<Pessoa> pLista, string info)
+        {
+            lbResultado.Items.Clear();
+            lbResultado.Items.Add(info);
+            lbResultado.Items.Add("");
+            lbResultado.Items.Add("ID\tNome\tIdade\tSexo");
+            pLista.ForEach(delegate (Pessoa p)
+            {
+                lbResultado.Items.Add(p.ID + "\t" + p.Nome + "\t" + p.Idade + "\t" + p.Sexo);
+            });
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            CarregaLista();
+            Imprimir(pLista, "Mostrando a lista");
+        }
+
+        private void btnFiltro_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> filtarIdade = pLista.FindAll(delegate (Pessoa p)
+            {
+                return p.Idade > 30;
+            });
+
+            Imprimir(filtarIdade, "Pessoas +30 anos");
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> RemoveM = pLista;
+            RemoveM.RemoveAll(delegate (Pessoa p)
+            {
+                return p.Sexo == 'M';
+            });
+
+            Imprimir(RemoveM, "Removendo todas as pessoa do sexo masculino");
+        }
+
+        private void btnLocalizar_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> pessoas = pLista.FindAll(delegate (Pessoa p)
+            {
+                return p.Idade == 12;
+            });
+            
+            Imprimir(pessoas, "Pessoas com idade = 12");
+        }
+
+        private void btnLocalizar2_Click(object sender, EventArgs e)
+        {
+            Int32 Lid = Convert.ToInt32(txtId.Text);
+            Pessoa pessoas = pLista.Find(delegate (Pessoa p1)
+            {
+                return p1.ID == Lid;
+            });
+            lbResultado.Items.Add("Pessoa Localizada!");
+            lbResultado.Items.Add(pessoas.ID + "\t" + pessoas.Nome + "\t" + pessoas.Idade + "\t" + pessoas.Sexo);
+            txtNome.Text = pessoas.Nome;
+            txtIdade.Text = Convert.ToString(pessoas.Idade);
+            txtSexo.Text = Convert.ToString(pessoas.Sexo);
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> npessoa = pLista;
+            npessoa.Add(new Pessoa(){ ID = Convert.ToInt32(txtId.Text.Trim()), Nome = txtNome.Text.Trim(), Idade = Convert.ToSByte(txtIdade.Text.Trim()), Sexo = Convert.ToChar(txtSexo.Text.Trim()) });
+            Imprimir(npessoa, "Pessoa adicionada!");
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtIdade.Text = "";
+            txtSexo.Text = "";
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Int32 Lid = Convert.ToInt32(txtId.Text);
+            List<Pessoa> RemoveLista = pLista;
+            RemoveLista.RemoveAll(delegate (Pessoa p) { return p.ID == Lid; });
+            Imprimir(RemoveLista, "Pessoa removida!");
+        }
+
+        private void btnGravaBanco_Click(object sender, EventArgs e)
+        {
+            List<Pessoa> ex = pLista;
+            for (int i = 0; i < ex.Count; i++)
+            {
+                SqlCommand cmd = new SqlCommand("InserirPessoa", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", SqlDbType.Int).Value = ex[i].ID;
+                cmd.Parameters.AddWithValue("@nome", SqlDbType.NChar).Value = ex[i].Nome;
+                cmd.Parameters.AddWithValue("@idade", SqlDbType.SmallInt).Value = ex[i].Idade;
+                cmd.Parameters.AddWithValue("@sexo", SqlDbType.Char).Value = ex[i].Sexo;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            MessageBox.Show("Lista gravada com sucesso", "Gravação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            Int32 Lid = Convert.ToInt32(txtId.Text);
+            List<Pessoa> RemoveLista = pLista;
+            RemoveLista.RemoveAll(delegate (Pessoa p1) { return p1.ID == Lid; });
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = txtNome.Text.Trim();
+            pessoa.Idade = Convert.ToSByte(txtIdade.Text.Trim());
+            pessoa.Sexo = Convert.ToChar(txtSexo.Text.Trim());
+            Imprimir(RemoveLista, "Pessoa atualizada!");
+            lbResultado.Items.Insert(Lid+2, Lid + "\t" + pessoa.Nome + "\t" + pessoa.Idade + "\t" + pessoa.Sexo);
+        }
+    }
+}
